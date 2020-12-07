8e00fa658135fba1f85d44e437e59e10145f8fac
Casper Munk Christiansen <caspermunk@gmail.com>
2020-12-07 11:03:41 +0100

    secrets example added

---
+++ includes/secrets.example.php
@@ -0,0 +1,8 @@
+<?php
+$secrets['password']        = '';
+$secrets['deploy_key']      = '';
+$secrets['mysql_host']      = '';
+$secrets['mysql_username']  = '';
+$secrets['mysql_password']  = '';
+$secrets['mysql_database']  = '';
+?>
