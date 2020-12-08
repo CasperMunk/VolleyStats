becba21cb2f5147f4898654065bd774e0cee50d2
Casper Munk Christiansen <caspermunk@gmail.com>
2020-12-08 16:44:09 +0100

    Indsamling af yderligere info

--- includes/volleystats.php
+++ includes/volleystats.php
@@ -51,12 +51,7 @@ class VolleyStats {
             (SELECT COUNT(*) FROM player_stats) as player_stats
         ";
 
-        if ($result = $this->db->query($query)) {
-            if ($result->num_rows>0){
-                $numbers = $result->fetch_all(MYSQLI_ASSOC)[0];
-                return $this->formatNumber($numbers);
-            }
-        }
+        return $this->formatNumber($this->fetchMysqlAll($query)[0]);
     }
 
     function formatNumber($val,$decimal=0)
@@ -91,7 +86,7 @@ class VolleyStats {
         }    
 
         foreach ($all_games as $game){
-            $this->db->query("REPLACE INTO games (id,competition_id) VALUES ($game,$competition_id)");
+            $this->executeMysql("REPLACE INTO games (id,competition_id) VALUES ($game,$competition_id)");
         }
 
         return $all_games;
@@ -123,9 +118,34 @@ class VolleyStats {
         if (strpos($content, '_DIV_MatchStats') == false) {
             return 'skipped';
         }else{
+            //Get general info about game
+            $game_data = array();
+
+            // $federation_id = preg_match_all("\d*(?=.*[0-9])\.pdf",$content,$matches);
+            // $game_data["federation_id"] = str_replace(".pdf","",);
+
+            $game_data["location"] = "'".$this->cleanInputData($this->getTagById("span","Content_Main_LB_Stadium",$content),"text")."'";
+
+            $game_data["spectators"] = $this->cleanInputData($this->getTagById("span","Content_Main_LBL_Spectators",$content));
+
+            $game_data["home_sets"] = $this->cleanInputData($this->getTagById("span","Content_Main_LBL_WonSetHome",$content));
+
+            $game_data["guest_sets"] = $this->cleanInputData($this->getTagById("span","Content_Main_LBL_WonSetGuest",$content));
+
+            //Get set scores an loop through them
+            $set_scores = explode(" ",trim($this->cleanInputData($this->getTagById("span","Content_Main_LB_SetsPartials",$content),"text")));
+
+            $set_count = 1;
+            foreach ($set_scores as $score){
+                $game_data["home_set".$set_count] = explode("/",$score)[0];
+                $game_data["guest_set".$set_count] = explode("/",$score)[1];
+                $set_count++;
+            }
+
+            //Get player statistics
             foreach (array("Home","Guest") as $team_type){
                 //Get team name
-                $team_name = strip_tags($this->getTagById("span","Content_Main_ctl17_RP_MatchStats_TeamName_".$team_type."_0",$content));
+                $team_name = $this->cleanInputData($this->getTagById("span","Content_Main_ctl17_RP_MatchStats_TeamName_".$team_type."_0",$content),"text");
 
                 //Get team id (create one if it doesn't exist)
                 $team_id = $this->getTeamId($team_name,$competition_id);
@@ -133,6 +153,8 @@ class VolleyStats {
                     echo 'No team ID for game no. '.$game_id;
                     exit;
                 }
+
+                $game_data[strtolower($team_type)."_team_id"] = $team_id;
 
                 //Load stat table
                 $team_table = $this->getTagById("div","RG_".$team_type."Team",$content);
@@ -153,8 +175,22 @@ class VolleyStats {
                     return false;
                 }
             }
+
+            //Update general info about game
+            $this->updateGameData($game_id,$game_data);
+
             return true;
         } 
+    }
+
+    function updateGameData($game_id,$dataset){
+        $update_fields = array();
+        foreach ($dataset as $key => $data){
+            $update_fields[] = $key." = ".$data;
+        }
+
+        $query = "UPDATE games SET ".implode(",",$update_fields)." WHERE id = ".$game_id;
+        $this->executeMysql($query);
     }
 
     function getHtmlData($url){
@@ -189,44 +225,48 @@ class VolleyStats {
     function addPlayerStats($game_id,$team_id,$player_id,$data){
         if (empty($player_id) OR empty($game_id)) return false;
 
-        $points_total =     $this->cleanStatsData($data->find("span[id=PointsTot]",0)->plaintext);
-        $break_points =     $this->cleanStatsData($data->find("span[id=Points]",0)->plaintext);
-        $serve_total =      $this->cleanStatsData($data->find("span[id=ServeTot]",0)->plaintext);
-        $serve_error =      $this->cleanStatsData($data->find("span[id=ServeErr]",0)->plaintext);
-        $serve_ace =        $this->cleanStatsData($data->find("span[id=ServeAce]",0)->plaintext);
-        $receive_total =    $this->cleanStatsData($data->find("span[id=RecTot]",0)->plaintext);
-        $receive_error =    $this->cleanStatsData($data->find("span[id=RecErr]",0)->plaintext);
+        $points_total =     $this->cleanInputData($data->find("span[id=PointsTot]",0)->plaintext);
+        $break_points =     $this->cleanInputData($data->find("span[id=Points]",0)->plaintext);
+        $serve_total =      $this->cleanInputData($data->find("span[id=ServeTot]",0)->plaintext);
+        $serve_error =      $this->cleanInputData($data->find("span[id=ServeErr]",0)->plaintext);
+        $serve_ace =        $this->cleanInputData($data->find("span[id=ServeAce]",0)->plaintext);
+        $receive_total =    $this->cleanInputData($data->find("span[id=RecTot]",0)->plaintext);
+        $receive_error =    $this->cleanInputData($data->find("span[id=RecErr]",0)->plaintext);
 
         $RecPos = $data->find("span[id=RecPos]",0);
-        if (isset($RecPos)) $receive_position = $this->cleanStatsData($RecPos->plaintext);
+        if (isset($RecPos)) $receive_position = $this->cleanInputData($RecPos->plaintext);
 
         $RecPos0 = $data->find("span[id=RecPos0]",0);
-        if (isset($RecPos0)) $receive_position = $this->cleanStatsData($RecPos0->plaintext);
+        if (isset($RecPos0)) $receive_position = $this->cleanInputData($RecPos0->plaintext);
 
         $receive_position = $receive_total * ($receive_position/100);
 
         $RecPerf = $data->find("span[id=RecPerf]",0);
-        if (isset($RecPerf)) $receive_perfect = $this->cleanStatsData($RecPerf->plaintext);
+        if (isset($RecPerf)) $receive_perfect = $this->cleanInputData($RecPerf->plaintext);
 
         $RecPerf0 = $data->find("span[id=RecPerf0]",0);
-        if (isset($RecPerf0)) $receive_perfect = $this->cleanStatsData($RecPerf0->plaintext);
+        if (isset($RecPerf0)) $receive_perfect = $this->cleanInputData($RecPerf0->plaintext);
 
         $receive_perfect = $receive_total * ($receive_perfect/100);
 
-        $spike_total =      $this->cleanStatsData($data->find("span[id=SpikeTot]",0)->plaintext);
-        $spike_error =      $this->cleanStatsData($data->find("span[id=SpikeErr]",0)->plaintext);
-        $spike_blocked =    $this->cleanStatsData($data->find("span[id=SpikeHP]",0)->plaintext);
-        $spike_win =        $this->cleanStatsData($data->find("span[id=SpikeWin]",0)->plaintext);
-        $block_win =        $this->cleanStatsData($data->find("span[id=BlockWin]",0)->plaintext);
-        $win_loss =         $this->cleanStatsData($data->find("span[id=L_VP]",0)->plaintext);
+        $spike_total =      $this->cleanInputData($data->find("span[id=SpikeTot]",0)->plaintext);
+        $spike_error =      $this->cleanInputData($data->find("span[id=SpikeErr]",0)->plaintext);
+        $spike_blocked =    $this->cleanInputData($data->find("span[id=SpikeHP]",0)->plaintext);
+        $spike_win =        $this->cleanInputData($data->find("span[id=SpikeWin]",0)->plaintext);
+        $block_win =        $this->cleanInputData($data->find("span[id=BlockWin]",0)->plaintext);
+        $win_loss =         $this->cleanInputData($data->find("span[id=L_VP]",0)->plaintext);
 
-        if ($result = $this->db->query("REPLACE INTO player_stats (player_id,game_id,team_id,points_total,break_points,serve_total,serve_error,serve_ace,receive_total,receive_error,receive_position,receive_perfect,spike_total,spike_error,spike_blocked,spike_win,block_win,win_loss) VALUES ($player_id,$game_id,$team_id,$points_total,$break_points,$serve_total,$serve_error,$serve_ace,$receive_total,$receive_error,$receive_position,$receive_perfect,$spike_total,$spike_error,$spike_blocked,$spike_win,$block_win,$win_loss)") === TRUE){
-            return true;
-        }
+        $this->executeMysql("REPLACE INTO player_stats (player_id,game_id,team_id,points_total,break_points,serve_total,serve_error,serve_ace,receive_total,receive_error,receive_position,receive_perfect,spike_total,spike_error,spike_blocked,spike_win,block_win,win_loss) VALUES ($player_id,$game_id,$team_id,$points_total,$break_points,$serve_total,$serve_error,$serve_ace,$receive_total,$receive_error,$receive_position,$receive_perfect,$spike_total,$spike_error,$spike_blocked,$spike_win,$block_win,$win_loss)");
     }
 
-    function cleanStatsData($val){
-        return intval(trim(str_replace('%', '', $val)));
+    function cleanInputData($val,$type="int"){
+        $val = strip_tags($val);
+        if ($type == "int"){
+            return intval(preg_replace('/\D/', '',$val));
+        }else{
+            return $val;
+        }
+        
     }
 
     function getPlayerId($player_name,$gender){
@@ -238,10 +278,9 @@ class VolleyStats {
         $player_id = array_search($player_name,$this->player_list,true);
         if ($player_id == false){
             //Add new player
-            if ($result = $this->db->query("INSERT INTO players (player_name,gender) VALUES ('$player_name','$gender')") === TRUE){
-                $player_id = $this->db->insert_id;
-                $this->player_list[$player_id] = $player_name;
-            }
+            $query = "INSERT INTO players (player_name,gender) VALUES ('$player_name','$gender')";
+            $player_id = $this->executeMysql($query);
+            $this->player_list[$player_id] = $player_name;
         }
 
         return $player_id;
@@ -325,7 +364,24 @@ class VolleyStats {
         if ($result = $this->db->query($query)) {
             if ($result->num_rows>0){
                 return $result->fetch_all(MYSQLI_ASSOC);
+            }else{
+                printf("MySQL Error: No rows returned. \n");
+                exit();
             }
+        }else{
+            printf("MySQL Error: %s\n", $this->db->error);
+            exit();
+        }
+    }
+
+    function executeMysql($query){
+        if ($result = $this->db->query($query) === TRUE){
+            if ($this->db->insert_id <> 0){
+                return $result->insert_id;
+            }
+        }else{
+            printf("MySQL Error: %s\n", $this->db->error);
+            exit();
         }
     }
 }
