09078936d3941fa572e840b2339232fd4f073766
Casper Munk Christiansen <caspermunk@gmail.com>
2020-12-14 02:27:04 +0100

    Update

--- includes/autoload.php
+++ includes/autoload.php
@@ -1,7 +1,9 @@
 <?php
 spl_autoload_register(function($className) {
 	$file = 'includes/classes/'.$className . '.php';
-	if (file_exists($file)) {
-		include $file;
+	if (is_file($file)) {
+		require_once($file);
+	}else{
+		echo 'Could not find class '.$className;
 	}
 });
