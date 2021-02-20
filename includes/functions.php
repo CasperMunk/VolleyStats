5b58246145bb2f91a100809d90dcf4b750aa5e80
Casper Munk Christiansen <caspermunk@gmail.com>
2021-02-21 00:44:35 +0100

    Updated passwords

--- includes/functions.php
+++ includes/functions.php
@@ -18,6 +18,7 @@ function post($name){
 function is_local() {
     if($_SERVER['HTTP_HOST'] == 'localhost'
         || substr($_SERVER['HTTP_HOST'],0,3) == '10.'
+        || substr($_SERVER['HTTP_HOST'],-6) == '.local'
         || substr($_SERVER['HTTP_HOST'],0,7) == '192.168') return true;
     return false;
 }
